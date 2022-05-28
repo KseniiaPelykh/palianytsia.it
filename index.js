3c41588cd841ba5929072cacd717b84b49ee8924
Kseniia Pelykh <kseniiapelykh@fb.com>
2022-05-28 19:42:01 +0100

    adding home page

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const path = require('path')
+const hoganMiddleware = require('hogan-middleware')
 
 const app = express()
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'mustache')
+app.engine('mustache', hoganMiddleware.__express)
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res, next) => {
-    res.send('Palianytsia!')
-})
+const indexRouter = require('./routes/index')
+app.use('/', indexRouter)
 
-app.listen(process.env.PORT)
+//app.listen(process.env.PORT)
+app.listen(3000)
